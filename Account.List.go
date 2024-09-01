93fdebd27404f0230a5f83a55fd152140d24e8ca
Sam Caldwell <github@samcaldwell.net>
2024-08-31 20:46:50 -0500

    minor fixes to List

--- Account.List.go
+++ Account.List.go
@@ -3,12 +3,12 @@ package quickbooks
 import "fmt"
 
 // List lists accounts in a paginated manner.
-func (acct *Account) List(client *Client, startPosition, maxResults int) ([]Account, error) {
+func (acct *Account) List(client *Client, startPosition, maxResults int) (AccountSet, error) {
 	endpoint := fmt.Sprintf("/account?startPosition=%d&maxResults=%d", startPosition, maxResults)
 	var result AccountSet
 	err := client.request("GET", endpoint, nil, &result)
 	if err != nil {
-		return nil, err
+		return result, err
 	}
-	return result.Accounts, nil
+	return result, nil
 }
