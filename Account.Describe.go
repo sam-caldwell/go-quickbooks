70e20ae851ce462ccf1964f6762f85790ed9546c
Sam Caldwell <github@samcaldwell.net>
2024-08-31 20:32:28 -0500

    code cleanup

--- Account.Describe.go
+++ Account.Describe.go
@@ -5,10 +5,9 @@ import "fmt"
 // Describe retrieves the details of an account matching the given accountId.
 func (acct *Account) Describe(client *Client, accountId string) (*Account, error) {
 	endpoint := fmt.Sprintf("/account/%s", accountId)
-	var result Account
-	err := client.request("GET", endpoint, nil, &result)
+	err := client.request("GET", endpoint, nil, acct)
 	if err != nil {
 		return nil, err
 	}
-	return &result, nil
+	return acct, nil
 }
